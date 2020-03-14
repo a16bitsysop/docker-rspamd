2c9d57f316ca34dc1f127aa60abdc235d0aa2a86
Duncan Bellamy <dunk@denkimushi.com>
2020-03-14 09:36:38 +0000

    move script into code block

--- get_latest.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo "$(wget -q https://registry.hub.docker.com/v1/repositories/$1/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | tail -n1)"

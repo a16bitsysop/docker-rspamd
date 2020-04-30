59d84f21beba102d0925d06a1d3cbd5e94c7c058
Duncan Bellamy <dunk@denkimushi.com>
2020-04-30 17:00:54 +0000

    wait for DNS to be ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,8 @@ echo
 
 if [ -n "$TIMEZONE" ]
 then
+  echo "Waiting for DNS"
+  ping -c1 -W60 google.com || ping -c1 -W60 www.google.com
   apk add --no-cache tzdata
   if [ -f /usr/share/zoneinfo/"$TIMEZONE" ]
   then

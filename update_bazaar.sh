90d847b3ca3a23517cf454648c782997e694c4fc
Duncan Bellamy <dunk@denkimushi.com>
2020-10-22 22:37:39 +0100

    fix linter errors

--- update_bazaar.sh
+++ update_bazaar.sh
@@ -7,7 +7,7 @@ do
   wget -O hash.zip https://bazaar.abuse.ch/export/txt/md5/full/
   unzip hash.zip
   cp -f full_md5.txt /etc/rspamd/local.d/maps.d/abuse_bazaar_full.txt
-  rm -rf *
+  rm -rf ./*
 
   ZZZ="${BZSLEEP-1}"
   ZZZ="$(echo "if ($ZZZ < 1) 1 else $ZZZ" | bc -l)"
